36cc1e8fe6775710ea1562c02094c628e1991091
Huy <quanghuy.nguyen2@griffithuni.edu.au>
2022-05-03 08:25:14 +1000

    Initial Commit

---
+++ Assignment2/Assignment2App.swift
@@ -0,0 +1,20 @@
+//
+//  Assignment2App.swift
+//  Assignment2
+//
+//  Created by Nguyen Quang Huy on 3/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct Assignment2App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
